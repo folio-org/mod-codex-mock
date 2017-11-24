32f88652170234841dada67a8fd681c9525e449d
Heikki Levanto <heikki@indexdata.dk>
2017-11-24 11:59:31 +0100

    Descriptors, simple run.sh

---
+++ run.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Simple test script for mod-codex-mock
+# Loads the module into Okapi, for the built-in supertenant
+
+
+# Parameters
+OKAPIPORT=9130
+OKAPIURL="http://localhost:$OKAPIPORT"
+CURL="curl -w\n -D - "
+TENANT="supertenant"
+TEN="-H X-Okapi-Tenant:$TENANT"
+
+# Check we have the fat jar
+if [ ! -f target/mod-codex-mock-fat.jar ]
+then
+  echo No fat jar found, no point in trying to run
+  exit 1
+fi
+
+# Start Okapi (in dev mode, no database)
+OKAPIPATH="../okapi/okapi-core/target/okapi-core-fat.jar"
+java -Dport=$OKAPIPORT -jar $OKAPIPATH dev > okapi.log 2>&1 &
+PID=$!
+echo Started okapi on port $OKAPIPORT. PID=$PID
+sleep 1 # give it time to start
+echo
+
+# Load the module
+echo "Loading mod-codex-mock"
+$CURL -X POST -d@target/ModuleDescriptor.json $OKAPIURL/_/proxy/modules
+echo
+
+echo "Deploying it"
+$CURL -X POST \
+   -d@target/DeploymentDescriptor.json \
+   $OKAPIURL/_/discovery/modules
+echo
+
+echo "Enabling it (without specifying the version)"
+$CURL -X POST \
+   -d'{"id":"mod-codex-mock"}' \
+   $OKAPIURL/_/proxy/tenants/$TENANT/modules
+echo
+sleep 1
+
+
+# Various tests
+echo Test 1: list the items
+$CURL $TEN \
+  $OKAPIURL/codex-instances
+echo
+
+# Various tests
+echo Test 2: Get one item
+$CURL $TEN \
+  $OKAPIURL/codex-instances/11111111-1111-1111-1111-111111111111
+echo
+
+# Let it run
+echo
+echo "Hit enter to close"
+read
+
+# Clean up
+echo "Cleaning up: Killing Okapi $PID"
+kill $PID
+ps | grep java && ( echo ... ; sleep 1  )
+ps | grep java && ( echo ... ; sleep 1  )
+ps | grep java && ( echo ... ; sleep 1  )
+ps | grep java && ( echo ... ; sleep 1  )
+ps | grep java && ( echo ... ; sleep 1  )
+rm -rf /tmp/postgresql-embed*
+ps | grep java && echo "OOPS - Still some processes running"
+echo bye
+
